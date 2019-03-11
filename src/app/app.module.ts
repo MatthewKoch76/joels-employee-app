89673258a991fcb6f718e923e2a3f4d6b958ca2b
MatthewKoch76 <37314071+MatthewKoch76@users.noreply.github.com>
2019-03-11 09:21:49 -0500

    material installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +7,7 @@ import { AppComponent } from './components/app/app.component';
 import { HomeComponent } from './components/home/home.component';
 import { TitleListComponent } from './components/title-list/title-list.component';
 import { SharedModule } from './shared/shared.module';
+import { MatToolbarModule, MatSidenavModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -15,8 +17,12 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
